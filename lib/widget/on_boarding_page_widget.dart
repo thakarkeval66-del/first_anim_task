2586c27f5eb7637ec14e8b942f987d28db67b2fd
keval <keval.thakar@discus.solutions>
2026-03-04 01:19:09 +0530

    -> animation logic of question transaction and question animation in implement in widget. -> animation logic of answer is also applied. -> animation logic of detail is also applied.

--- lib/widget/on_boarding_page_widget.dart
+++ lib/widget/on_boarding_page_widget.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:first_anim_task/model/on_boarding_model.dart';
 
 class OnboardingPage extends StatefulWidget {
@@ -67,6 +68,78 @@ class _OnboardingPageState extends State<OnboardingPage> {
     final contentTwoLineTop = centerY - 60;
     final contentTop = centerY - 50;
 
-    return Container();
+    return Container(
+      color: widget.data.backgroundColor,
+      width: double.infinity,
+      child: Stack(
+        children: [
+          AnimatedPositioned(
+            duration: const Duration(milliseconds: 400),
+            curve: Curves.easeInOutQuart,
+            left: horizontalPadding,
+            right: horizontalPadding,
+            top: !_isVisible
+                ? screenHeight
+                : (_isSettled ? finalTop : initialTop),
+            child: AnimatedOpacity(
+              duration: const Duration(milliseconds: 250),
+              opacity: _isVisible ? 1.0 : 0.0,
+              child: AnimatedDefaultTextStyle(
+                duration: const Duration(milliseconds: 400),
+                curve: Curves.easeInOutQuart,
+                style: GoogleFonts.lexend(
+                  color: _isSettled ? widget.data.questionTextColor : Colors.white,
+                  fontSize: _isSettled ? 26 : 30,
+                  fontWeight: FontWeight.w600,
+                  height: 1.0,
+                  letterSpacing: 0,
+                ),
+                child: Text(widget.data.question),
+              ),
+            ),
+          ),
+
+          AnimatedPositioned(
+            duration: const Duration(milliseconds: 500),
+            curve: Curves.easeOutQuart,
+            left: _isSettled ? horizontalPadding : screenWidth,
+            top: widget.twoLine?contentTwoLineTop:contentTop,
+            child: AnimatedOpacity(
+              duration: const Duration(milliseconds: 300),
+              opacity: _isSettled ? 1.0 : 0.0,
+              child: SizedBox(
+                width: screenWidth - (horizontalPadding * 2),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      widget.data.answer,
+                      style: GoogleFonts.lexend(
+                        color: Colors.white,
+                        fontSize: 50,
+                        fontWeight: FontWeight.w800,
+                        height: 1.0,
+                        letterSpacing: 0,
+                      ),
+                    ),
+                    const SizedBox(height: 12),
+                    Text(
+                      widget.data.detail,
+                      style: GoogleFonts.lexend(
+                        color: Colors.white,
+                        fontSize: 32,
+                        fontWeight: FontWeight.w800,
+                        height: 36 / 32,
+                        letterSpacing: 0,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
